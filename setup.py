f2775a0fbb77d716cec5754b25051738447a504f
Ford Scott <scottc99@students.rowan.edu>
2015-07-09 00:12:06 -0400

    finishing up setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#### System and module requirements for adsorption analysis software ####
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from distutils.command.install import install
+import os
+import sys
+
+
+setup(
+    name = 'ASC-Parser',
+    version = "0.0.1.dev1",
+    package_dir = {'': 'python'},
+    packages = find_packages(),
+    url = 'https://github.com/scottc99/ASC-Parser',
+    long_description = open('README.md').read()
+
+    # cmdclass = {'sdist': sdist_git},
+)
